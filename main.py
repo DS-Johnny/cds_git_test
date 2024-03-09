8c7376d3a3acfdcb486560fec2018a3fafda71df
Johnny Matos <johnny.datascientist@gmail.com>
2024-03-09 15:11:14 -0300

    Criado funcao de exibir mensagens

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import test
 def gether_data():
-    n1 = int(input("Primeiro valor: "))
-    n2 = int(input("Segundo valor: "))
+    n1 = input("Primeiro valor: ")
+    n2 = input("Segundo valor: ")
 
     return n1, n2
 
+def print_message(n1, n2):
+    print(f'Os valores {n1} e {n2} somados são: {n1+n2}')
+
+    return None
 
 def main():
     n1, n2 = gether_data()
 
-    print(n1**n2)
+    print_message(n1,n2)
 
     return None
 
